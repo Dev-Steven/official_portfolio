f2c61ead7c77ebb94f1fd25e824a52598c1e3767
Dev-Steven <stevenchung650@gmail.com>
2020-08-20 22:50:25 -0700

    padding adjustments

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,18 +6,20 @@ export default class HomePage extends Component {
 	render() {
 		return (
 			<div className='home-info text-center' id='home'>
-				<h1 className='font-weight-bold'>Steven Chung</h1>
-				<LightSpeedIn>
-					<h4 className='job-title'>Software Engineer</h4>
-				</LightSpeedIn>
-				<hr></hr>
-				<p className='message font-weight-normal'>
-					Hey, I'm Steven, a Software Engineer specializing in
-					Front-End development.
-				</p>
-				<a className='btn btn-outline-dark m-auto' href='#projects'>
-					Checkout my work
-				</a>
+				<div className='container'>
+					<h1 className='font-weight-bold'>Steven Chung</h1>
+					<LightSpeedIn>
+						<h4 className='job-title'>Software Engineer</h4>
+					</LightSpeedIn>
+					<hr></hr>
+					<p className='message font-weight-normal'>
+						Hey, I'm Steven, a Software Engineer specializing in
+						Front-End development.
+					</p>
+					<a className='btn btn-outline-dark m-auto' href='#projects'>
+						Checkout my work
+					</a>
+				</div>
 			</div>
 		);
 	}
